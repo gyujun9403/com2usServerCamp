52c60b4490a064af493c34ce77cc50b773efb0b2
gyeon <75626815+gyujun9403@users.noreply.github.com>
2023-04-27 18:36:52 +0900

    feat: 로그인 추가를 위한 redis연결 추가중

--- DungeonFarming/DungeonFarming/DataBase/GameSessionDb/IGameSessionDb.cs
+++ DungeonFarming/DungeonFarming/DataBase/GameSessionDb/IGameSessionDb.cs
@@ -2,5 +2,11 @@
 {
     public interface IGameSessionDb
     {
+        // token functions
+        Task<ErrorCode> setToken(AuthCheckModel mokdel);
+        Task<(ErrorCode, String)> getToken(String accountId);
+
+        // GameSession : 유저별 게임의 진행상태 / 직전 상태 에 대한 정보
     }
+
 }
--- DungeonFarming/DungeonFarming/Middleware/AuthCheckMiddleware.cs
+++ DungeonFarming/DungeonFarming/Middleware/AuthCheckMiddleware.cs
@@ -1,6 +1,20 @@
 ﻿namespace DungeonFarming.Middleware
 {
-    public class FileName
+    public class AuthCheckMiddleware
     {
+        private RequestDelegate _next;
+
+        public AuthCheckMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public void Invoke(HttpContext context)
+        {
+            String path = context.Request.Path;
+            if (!(path.StartsWith("/Registe") || path.StartsWith("/Login")))
+            {
+                // 토큰 검사 로직
+            }
+        }
     }
 }
