b264a7dbcb9158c822bec7882f5d4272ac509bb3
gyeon <75626815+gyujun9403@users.noreply.github.com>
2023-05-13 17:05:41 +0900

    session developing

--- DungeonFarming/DungeonFarming/DataBaseServices/GameSessionDb/GameSessionData.cs
+++ DungeonFarming/DungeonFarming/DataBaseServices/GameSessionDb/GameSessionData.cs
@@ -1,17 +1,19 @@
-﻿namespace DungeonFarming.DataBase.GameSessionDb
-{
-    public enum UserStatus : Int16
-    {
-        Login = 0,
-        Gaming = 1
-    }
-    public class GameSessionData
-    {
-        public String userId { get; set; }
+﻿namespace DungeonFarming.DataBase.GameSessionDb;
 
-        public Int64 pkId { get; set; }
-        public String token { get; set; }
-        //
-        public UserStatus userStatus { get; set; }
-    }
+public enum UserStatus : Int16
+{
+    Login = 0,
+    Gaming = 1
+}
+
+public class GameSessionData
+{
+    public String userId { get; set; }
+
+    public Int64 pkId { get; set; }
+    public String token { get; set; }
+    public UserStatus userStatus { get; set; }
+    public Int64 stageCode { get; set; }
+    public Dictionary<Int64, Int64> killedNpcs { get; set; }
+    public Dictionary<Int64, Int64> FarmedItems { get; set; }
 }
